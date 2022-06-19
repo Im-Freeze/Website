acebd4808d74e4ed34994a789c79fc4d4f17fa98
OLIMINATOR <oliminator@protonmail.com>
2022-06-19 18:47:19 +0200

    chore: image minify

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,37 @@
 import { defineConfig } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
+import viteImagemin from "vite-plugin-imagemin";
 import twig from "vite-plugin-twig";
 
 export default defineConfig({
     plugins: [
+        viteImagemin({
+            gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false,
+            },
+            optipng: {
+                optimizationLevel: 7,
+            },
+            mozjpeg: {
+                quality: 20,
+            },
+            pngquant: {
+                quality: [0.8, 0.9],
+                speed: 4,
+            },
+            svgo: {
+                plugins: [
+                    {
+                        name: "removeViewBox",
+                    },
+                    {
+                        name: "removeEmptyAttrs",
+                        active: false,
+                    },
+                ],
+            },
+        }),
         twig(),
         createHtmlPlugin({
             minify: true,
